cdc55f151366bb1aac8fda504efcab0dfd091c95
Graham Jarvis <zerothousand111@proton.me>
2025-08-24 22:21:17 +0100

    implemented assignment 7 - add comments to routes

--- todo.rb
+++ todo.rb
@@ -15,16 +15,19 @@ get "/" do
   redirect "/lists"
 end
 
+# View the list of lists
 get "/lists" do
   @lists = session[:lists]
   erb :lists, layout: :layout
 end
 
+# Create a new list
 post "/lists" do
   session[:lists] << {name: params[:list_name], todos: []}
   redirect "/lists"
 end
 
+# Render the new list form
 get "/lists/new" do
   erb :new_list, layout: :layout
 end
