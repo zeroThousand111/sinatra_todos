5242f8184558e9c72c6f6cdb4e1801b294a3cff4
Graham Jarvis <zerothousand111@proton.me>
2025-08-24 11:36:24 +0100

    implemented assignment 5 - viewing all todo lists

--- todo.rb
+++ todo.rb
@@ -2,6 +2,14 @@ require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubi"
 
-get "/" do
-  erb "You have no lists.", layout: :layout
+get "/" do 
+  redirect "/lists"
+end
+
+get "/lists" do
+  @lists = [
+    {name: "Lunch Groceries", todos: []},
+    {name: "Lunch Groceries", todos: []}
+  ]
+  erb :lists, layout: :layout
 end
