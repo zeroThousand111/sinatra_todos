1b1b307c3f434f70bb4c5ed4907465447fe85e3d
Graham Jarvis <zerothousand111@proton.me>
2025-08-24 12:57:54 +0100

    implemented creating a new todo list feature from assignment 6

--- todo.rb
+++ todo.rb
@@ -2,14 +2,29 @@ require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubi"
 
+configure do
+  enable :sessions
+  set :session_secret, SecureRandom.hex(32)
+end
+
+before do
+  session[:lists] ||= []
+end
+
 get "/" do 
   redirect "/lists"
 end
 
 get "/lists" do
-  @lists = [
-    {name: "Lunch Groceries", todos: []},
-    {name: "Lunch Groceries", todos: []}
-  ]
+  @lists = session[:lists]
   erb :lists, layout: :layout
+end
+
+post "/lists" do
+  session[:lists] << {name: params[:list_name], todos: []}
+  redirect "/lists"
+end
+
+get "/lists/new" do
+  erb :new_list, layout: :layout
 end
