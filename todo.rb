bbd22e81e541f31e0d7ce6c77dac5ca1abcc2878
Graham Jarvis <zerothousand111@proton.me>
2025-08-26 10:54:57 +0100

    implemented assignment 10 - created validation that list name is between 1-100 chars

--- todo.rb
+++ todo.rb
@@ -23,9 +23,15 @@ end
 
 # Create a new list
 post "/lists" do
-  session[:lists] << {name: params[:list_name], todos: []}
-  session[:success] = "The list has been created."
-  redirect "/lists"
+  list_name = params[:list_name].strip
+  if (1..100).cover? list_name.size
+    session[:lists] << {name: list_name, todos: []}
+    session[:success] = "The list has been created."
+    redirect "/lists"
+  else
+    session[:error] = "List name must be between 1 and 100 characters."
+    erb :new_list, layout: :layout
+  end
 end
 
 # Render the new list form
