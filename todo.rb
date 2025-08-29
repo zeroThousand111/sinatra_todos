daaaf25f172c14f6617243d41c326b5818bd31f6
Graham Jarvis <zerothousand111@proton.me>
2025-08-29 20:20:40 +0100

    implemented assignment 22 - completing all todos on a list

--- todo.rb
+++ todo.rb
@@ -134,3 +134,16 @@ post "/lists/:list_id/todos/:id" do
   session[:success] = "The todo has been updated."
   redirect "/lists/#{@list_id}"
 end
+
+# Mark all todos complete for a list
+post "/lists/:id/complete_all" do
+  @list_id = params[:id].to_i
+  @list = session[:lists][@list_id]
+
+  @list[:todos].each do |todo|
+    todo[:completed] = true
+  end
+
+  session[:success] = "All todos have been completed."
+  redirect "/lists/#{@list_id}"
+end
