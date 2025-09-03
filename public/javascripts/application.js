f643e61dd42d41c6f34bc145a9e545d1f987bda7
Graham Jarvis <zeroThousand111@proton.me>
2025-09-03 10:51:15 +0100

    implemented assignment 4 lesson 7 RB175 - added js to confirm destructive actions

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,1 +1,13 @@
-console.log("this is a test");
+$(function() {
+
+  $("form.delete").submit(function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    var ok = confirm("Are you sure? This cannot be undone!");
+    if (ok) {
+      this.submit();
+    }
+  });
+
+});
